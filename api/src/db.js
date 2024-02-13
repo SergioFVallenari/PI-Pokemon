2954914b36cf47fe3d440287b350cee147b29276
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-02-13 03:32:26 -0300

    Actualizacion para el deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_URL } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`,
+   DB_URL,
    {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      
+      logging: false, 
+      native: false, 
+      ssl: false,
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false,
+         },
+      },
    }
 );
 const basename = path.basename(__filename);
