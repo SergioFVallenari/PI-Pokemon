db0e735d4a640010f376bf6203dc8d62ea29a47d
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-02-13 14:27:13 -0300

    Prueba 2 deploy

--- api/src/handlers/allPokemonsHandlers.js
+++ api/src/handlers/allPokemonsHandlers.js
@@ -2,7 +2,7 @@ const getPokemons = require("../controllers/getPokemons")
 
 const allPokemonsHandler = async (req, res) => {
     try {
-        const allPokemons = await getPokemons()
+        const allPokemons = await Promise.all(getPokemons())
         res.status(200).json(allPokemons)
     } catch (error) {
         res.status(500).send(error)
